78dc2fd9f4240b00354c7473d6704f024d6fd3d6
teddy syaiful <tedsse@gmail.com>
2024-11-04 09:43:03 +0700

    Improve error logging and troubleshoot DB connection

--- db.js
+++ db.js
@@ -1,20 +1,21 @@
-// db.js
+require('dotenv').config();
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST || '34.128.66.32',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASS || 'L#.J:ySFyHN,&Fit',
-    database: process.env.DB_NAME || 'db_register'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
 });
 
 db.connect((err) => {
-    if (err) {
-        console.error('Error connecting to the database:', err);
-        process.exit(1);
-    } else {
-        console.log('Connected to the database');
-    }
+  if (err) {
+      console.error('Database connection error:', err);
+      process.exit(1); // Terminate the process if unable to connect
+  } else {
+      console.log('Connected to the database');
+  }
 });
+
 
 module.exports = db;
