5cad2bfee26c3401cf06170a3d04e4740cc6200f
teddy syaiful <tedsse@gmail.com>
2024-11-04 09:26:11 +0700

    Connect to Google Cloud SQL

--- db.js
+++ db.js
@@ -2,15 +2,19 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '123456',
-  database: 'db_register'
+    host: process.env.DB_HOST || '34.128.66.32',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASS || 'L#.J:ySFyHN,&Fit',
+    database: process.env.DB_NAME || 'db_register'
 });
 
 db.connect((err) => {
-  if (err) throw err;
-  console.log('Connected to database');
+    if (err) {
+        console.error('Error connecting to the database:', err);
+        process.exit(1);
+    } else {
+        console.log('Connected to the database');
+    }
 });
 
 module.exports = db;
